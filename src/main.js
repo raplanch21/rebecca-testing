139be7c1586626c293c4fc44d92acd5b67d1d4a0
raplanch21 <rebecca.planchart@pendo.io>
2026-06-22 09:43:54 -0400

    Revert "Merge pull request #4 from raplanch21/novus/install-pendo-sdk"
    This reverts commit 065d57f97a2a9bd0a92ad8dceff240e3b0a000af, reversing
    changes made to 2f0a4c360f3f254fc662f144965047bd77ccf62d.

--- src/main.js
+++ src/main.js
@@ -4,12 +4,6 @@ import router from './router'
 import './index.css'
 import HighchartsVue from 'highcharts-vue'
 
-pendo.initialize({
-  visitor: {
-    id: ''
-  }
-});
-
 const app = createApp(App)
 app.use(router)
 app.use(HighchartsVue)
